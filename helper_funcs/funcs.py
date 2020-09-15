7d97b178852a1e88d6e164b2c19af1a8cc31f2da
capac <capac@users.noreply.github.com>
2020-09-15 16:56:45 +0100

    Modified print_accuracy function to output accuracy and calssification if initial_time != None and print_output=True

--- helper_funcs/funcs.py
+++ helper_funcs/funcs.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score, classification_report, f1_score
 from collections import namedtuple
 from time import time
 import sys
+import numpy as np
 
 
 def clf_func(clf_dict):
@@ -16,12 +17,14 @@ def clf_func(clf_dict):
     return clf_list
 
 
-def print_accuracy(model_clf, t, X_test, y_test):
+def print_accuracy(model_clf, X_test, y_test, initial_time=None, print_output=False):
+    # NOTICE: returns valid print statement only if initial_time != None and print_output=True
     y_pred = model_clf.predict(X_test)
     acc_score = accuracy_score(y_pred, y_test)
-    sys.stdout.write(f'\nAccuracy score for {model_clf.__class__.__name__}: {acc_score:.6f}')
-    sys.stdout.write(f'\nTime elapsed: {time() - t:.4f} sec')
-    sys.stdout.write(f'\nClassification report:\n{classification_report(y_pred, y_test, digits=4)}')
+    if print_output:
+        sys.stdout.write(f'\nAccuracy score for {model_clf.__class__.__name__}: {acc_score:.6f}')
+        sys.stdout.write(f'\nTime elapsed: {time() - initial_time:.4f} sec')
+        sys.stdout.write(f'\nClassification report:\n{classification_report(y_pred, y_test, digits=4)}')
     return acc_score
 
 
@@ -34,13 +37,14 @@ def run_clf(X_train, X_test, y_train, y_test, clf_list, model_dir):
         if model_file in list_files:
             t1 = time()
             model_clf = load(model_file)
-            accuracy_score_dict[model_clf.__class__.__name__] = print_accuracy(model_clf, t1, X_test, y_test.values)
+            accuracy_score_dict[model_clf.__class__.__name__] = print_accuracy(model_clf, X_test, y_test.values, initial_time=t1, print_output=True)
         else:
             t2 = time()
             item.classifier.fit(X_train, y_train.values)
             dump(item.classifier, model_file)
-            accuracy_score_dict[item.classifier.__class__.__name__] = print_accuracy(item.classifier, t2, X_test, y_test.values)
+            accuracy_score_dict[item.classifier.__class__.__name__] = print_accuracy(item.classifier, t2, X_test, y_test.values, initial_time=t2, print_output=True)
     print(f'\nTotal time elasped: {time() - t0:.4f} sec')
+    return accuracy_score_dict
 
 
 def grid_search_func(X_train, y_train, grid_search, joblib_file):
@@ -54,3 +58,12 @@ def grid_search_func(X_train, y_train, grid_search, joblib_file):
     print(f'Best estimator for grid search: {grid_search.best_estimator_}\n')
     print(f'Time elapsed: {time() - t0:.4f} sec')
     return grid_search
+
+
+def grid_results(grid_search, num):
+    cvres = grid_search.cv_results_
+    best_fit_models = [(np.sqrt(-mean_score), params) for mean_score, params in zip(cvres['mean_test_score'], cvres['params'])]
+    best_fit_models.sort(key=lambda x: x[0], reverse=False)
+    print(f'List of best-fit models sorted by RMSE:')
+    for rmse, params in best_fit_models[:num]:
+        print(f'{rmse} {params}')
